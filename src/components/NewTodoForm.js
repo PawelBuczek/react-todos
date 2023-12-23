fc580b51c6004809e046e0a1d91aed6df261c2fb
PawelBuczek <pawel.buczek@email.com>
2023-12-23 13:46:04 +0100

    adding todo functionality

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function NewTodoForm() {
+function NewTodoForm(props) {
 
     const [description, setDescription] = useState('');
     const [assigned, setAssigned] = useState('');
@@ -13,32 +13,44 @@ function NewTodoForm() {
     const assignedChange = (event) => {
         console.log('assigned', event.target.value);
         setAssigned(event.target.value);
-    } 
+    }
+
+    const submitTodo = () => {
+        if (description !== '' && assigned !== '') {
+            props.addTodo(description, assigned);
+            setDescription('');
+            setAssigned('');
+        }
+    }
 
     return (
         <div className='mt-5'>
             <form>
                 <div className='mb-3'>
                     <label className='form-label'>Assigned</label>
-                    <input 
-                    type='text' 
-                    className='form-control' 
-                    required
-                    onChange={assignedChange}
-                    value={assigned}
+                    <input
+                        type='text'
+                        className='form-control'
+                        required
+                        onChange={assignedChange}
+                        value={assigned}
                     ></input>
                 </div>
                 <div className='mb-3'>
                     <label className='form-label'>Description</label>
-                    <textarea 
-                    className='form-control' 
-                    rows={3} 
-                    required
-                    onChange={e => setDescription(e.target.value)}
-                    value={description}
+                    <textarea
+                        className='form-control'
+                        rows={3}
+                        required
+                        onChange={e => setDescription(e.target.value)}
+                        value={description}
                     ></textarea>
                 </div>
-                <button type='button' className='btn btn-primary mt-3'>Add Todo</button>
+                <button
+                    type='button'
+                    className='btn btn-primary mt-3'
+                    onClick={submitTodo}
+                >Add Todo</button>
             </form>
         </div>
     )
