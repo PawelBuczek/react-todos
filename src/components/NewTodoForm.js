838047f25ac1d37f3a9c7c3af115c78784831cc3
PawelBuczek <pawel.buczek@email.com>
2023-12-23 12:53:53 +0100

    assigning values

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function NewTodoForm() {
 
-    const descriptionChange = (event) => {
-        console.log('description', event.target.value);
-    } 
+    const [description, setDescription] = useState('');
+    const [assigned, setAssigned] = useState('');
+
+    // const descriptionChange = (event) => {
+    //     console.log('description', event.target.value);
+    //     setDescription(event.target.value);
+    // } 
 
     const assignedChange = (event) => {
         console.log('assigned', event.target.value);
+        setAssigned(event.target.value);
     } 
 
     return (
@@ -20,6 +25,7 @@ function NewTodoForm() {
                     className='form-control' 
                     required
                     onChange={assignedChange}
+                    value={assigned}
                     ></input>
                 </div>
                 <div className='mb-3'>
@@ -28,7 +34,8 @@ function NewTodoForm() {
                     className='form-control' 
                     rows={3} 
                     required
-                    onChange={descriptionChange}
+                    onChange={e => setDescription(e.target.value)}
+                    value={description}
                     ></textarea>
                 </div>
                 <button type='button' className='btn btn-primary mt-3'>Add Todo</button>
